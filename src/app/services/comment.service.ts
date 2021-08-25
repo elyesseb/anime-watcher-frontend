cadc6556abe3d2d12e00b05116829a50aa6d0e90
DESKTOP-R7U4PP7\ufhopla <elyesse.benhadj@gmail.com>
2021-08-25 15:39:26 +0200

    fixing service

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -2,12 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+const baseUrl = 'http://localhost:8080/comment';
+
 @Injectable({
   providedIn: 'root'
 })
-
-const baseUrl = 'http://localhost:8080/comment';
-
 
 export class CommentService {
 
