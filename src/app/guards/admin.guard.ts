1f162131b9304fc2fe28328b8e4cd582dbd119c2
ufhopla <elyesse.benhadj@gmail.com>
2022-10-30 02:34:28 +0100

    test guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenStorageService } from '../services/token-storage.service';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-  constructor(private router : Router, private token: TokenStorageService) { }
+  constructor(private router : Router, private token: TokenStorageService, private user:UserService) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -20,21 +21,38 @@ export class AdminGuard implements CanActivate {
       const role = this.token.getUser().roles;
       const admin = "ROLE_ADMIN";
       let result = '';
-
-      for (let i = 0; i < role.length; i++) {
-        if(role[i] === admin) {
-          return true;
-        } else {
-          this.router.navigate(['notfound']);
-          return false;
+      let res;
+      this.user.getAdminBoard().subscribe(
+        (data) => {
+          console.log(data);
+          res = data;
+        },
+        (err) => {
+          console.error(err);
         }
-      }
-
-      if (result === admin) {
+      );
+      if(res){
+        console.log(res);
         return true;
       } else {
         this.router.navigate(['notfound']);
         return false;
       }
+
+      // for (let i = 0; i < role.length; i++) {
+      //   if(role[i] === admin) {
+      //     return true;
+      //   } else {
+      //     this.router.navigate(['notfound']);
+      //     return false;
+      //   }
+      // }
+
+      // if (result === admin) {
+      //   return true;
+      // } else {
+      //   this.router.navigate(['notfound']);
+      //   return false;
+      // }
     }
   }
