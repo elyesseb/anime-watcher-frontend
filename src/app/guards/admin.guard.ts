6e630f25a3a983ce8ee9bf510ac1c0388f56e0fc
ufhopla <elyesse.benhadj@gmail.com>
2022-10-30 12:58:18 +0100

    Fix guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenStorageService } from '../services/token-storage.service';
 import { UserService } from '../services/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(private router : Router, private token: TokenStorageService, private user:UserService) { }
+  constructor(
+    private router: Router,
+    private token: TokenStorageService,
+    private user: UserService
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -17,41 +27,41 @@ export class AdminGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-      // const role = this.token.getUser().roles;
-      // const admin = "ROLE_ADMIN";
-      // let result = '';
-      let res;
-      this.user.getAdminBoard().subscribe(
-        (data) => {
-          console.log(data);
-          res = data;
-        },
-        (err) => {
-          console.error(err);
-        }
-      );
-      if(res === "Admin Board."){
-        console.log(res);
+    const role = this.token.getUser().roles;
+    const admin = 'ROLE_ADMIN';
+    let result = '';
+    // let res;
+    // this.user.getAdminBoard().subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //     res = data;
+    //   },
+    //   (err) => {
+    //     console.error(err);
+    //   }
+    // );
+    // if(res === "Admin Board."){
+    //   console.log(res);
+    //   return true;
+    // } else {
+    //   this.router.navigate(['notfound']);
+    //   return false;
+    // }
+
+    for (let i = 0; i < role.length; i++) {
+      if (role[i] === admin) {
         return true;
       } else {
         this.router.navigate(['notfound']);
         return false;
       }
+    }
 
-      // for (let i = 0; i < role.length; i++) {
-      //   if(role[i] === admin) {
-      //     return true;
-      //   } else {
-      //     this.router.navigate(['notfound']);
-      //     return false;
-      //   }
-      // }
-
-      // if (result === admin) {
-      //   return true;
-      // } else {
-      //   this.router.navigate(['notfound']);
-      //   return false;
-      // }
+    if (result === admin) {
+      return true;
+    } else {
+      this.router.navigate(['notfound']);
+      return false;
     }
   }
+}
