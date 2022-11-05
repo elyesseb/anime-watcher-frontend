216e3cd7aecacb551f7272e52043ca32d631a58c
ufhopla <elyesse.benhadj@gmail.com>
2022-11-05 19:50:14 +0100

    Fix file upload uri

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://snk-api.azurewebsites.net';
 
 
 
